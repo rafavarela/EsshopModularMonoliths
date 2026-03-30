29dc843d4aaaeaa29947effcab3e34e9832c0696
Rafael Varela <rafa_varela@yahoo.com>
2026-03-30 18:07:33 -0500

    Introduce DDD base and product aggregate with events
    Added DDD abstractions (Entity, Aggregate, IDomainEvent) and implemented the Product aggregate root with creation and update logic. Introduced ProductCreatedEvent and ProductPriceChangedEvent. Integrated MediatR for domain event handling.

---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Shared.DDD;
+
+public interface IDomainEvent : INotification
+{
+    Guid EvenntId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
